8fce28451a358960b6274612cc9141df63dafe29
Naveen Chintalapudi <naveen.chintalapudi@popcornapps.com>
2023-12-06 13:17:24 +0530

    CPPointOfInterestTemplete added

--- SampleAccelloriOSFramework/CarPlaySceneDelegate.swift
+++ SampleAccelloriOSFramework/CarPlaySceneDelegate.swift
@@ -2,87 +2,60 @@ import CarPlay
 import UIKit
 import MapKit
 
-public class CarPlaySceneDelegate: UIResponder  {
-
+public class CarPlaySceneDelegate: UIResponder, CPTemplateApplicationSceneDelegate, CPPointOfInterestTemplateDelegate  {
+   
     // MARK: - Properties
     var interfaceController: CPInterfaceController?
     var raceTracksInfo = [RaceTracLocationInfo]()
-    
+
     let listViewTemplate: CPListTemplate = CPListTemplate(title: "Locations", sections: [])
     
+    public func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene,
+        didConnect interfaceController: CPInterfaceController) {
+    
+        self.interfaceController = interfaceController
+        self.interfaceController?.delegate = self
 
+        // Notifications
+        NotificationCenter.default.addObserver(self, selector: #selector(handleSelectedObject(_:)),
+                name: Notification.Name("SampleAccelloriOSFrameworkDidSelectRow"), object: nil)
+        self.getNewData()
+        listViewTemplate.updateSections([updateRaceTracksList()])
+        listViewTemplate.tabTitle = "Locations"
+        let tabBar = CPTabBarTemplate.init(templates: [listViewTemplate ])
+        tabBar.delegate = self
+        self.interfaceController?.setRootTemplate(tabBar, animated: true, completion: {_, _ in })
+    }
+    
+    public func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene, didDisconnectInterfaceController interfaceController: CPInterfaceController) {
+    
+        self.interfaceController = nil
+    }
+    
+    func getNewData(){
+        let locationsDataString = UserDefaults.standard.string(forKey: "LOCATIONS_DATA")
+
+        if let reversedData = locationsDataString!.data(using: .utf8),
+            let reversedArray = try? JSONDecoder().decode([RaceTracLocationInfo].self, from: reversedData) {
+            self.raceTracksInfo = reversedArray
+        }
+
+    }
+    
     // MARK: - Custom Methods
     func updateRaceTracksList() -> CPListSection {
         var raceTrackItems = [CPListItem]()
         for raceTrack in raceTracksInfo {
             let item = CPListItem(text: raceTrack.city, detailText: raceTrack.address)
             item.accessoryType = .none
-            // item.setImage(UIImage(named: radio.imageSquareUrl))
             item.handler = { [weak self] item, completion in
                 guard let strongSelf = self else { return }
                 SampleAccelloriOSFramework.shared.sendObjectToMainProject(selectedObject: raceTrack)
                 strongSelf.selectedPop(raceTrack: raceTrack, completion: completion)
-                // strongSelf.popList(raceTrack: raceTrack, completion: completion)
             }
             raceTrackItems.append(item)
         }
         return CPListSection(items: raceTrackItems)
-    }
-    
-//    func popList(raceTrack: RaceTracLocationInfo, completion: @escaping () -> Void) {
-//        let okAlertAction: CPAlertAction = CPAlertAction(title: "Ok", style: .default) { _ in
-//            SampleAccelloriOSFramework.shared.sendObjectToMainProject(selectedObject: raceTrack)
-//            self.interfaceController?.dismissTemplate(animated: true, completion: { _, _ in })
-//        }
-//        let titleAlert =  "Send to Device"
-//        let alertTemplate: CPAlertTemplate = CPAlertTemplate(titleVariants: [titleAlert], actions: [okAlertAction])
-//        self.interfaceController?.presentTemplate(alertTemplate, animated: true, completion: { _, _ in
-//            completion()
-//        })
-//    }
-    
-    func selectedPop(raceTrack: RaceTracLocationInfo, completion: @escaping () -> Void) {
-        let okAlertAction: CPAlertAction = CPAlertAction(title: "Navigate", style: .default) { _ in
-            self.interfaceController?.dismissTemplate(animated: true, completion: { _, _ in })
-            self.openMapForPlace(raceTrack: raceTrack)
-        }
-        let titleAlert = raceTrack.city + "," + raceTrack.address + "," + "Store " + raceTrack.storeId + "," + String(raceTrack.lat) + "," + String(raceTrack.log)
-        let alertTemplate: CPAlertTemplate = CPAlertTemplate(titleVariants: [titleAlert], actions: [okAlertAction])
-        self.interfaceController?.presentTemplate(alertTemplate, animated: true, completion: { _, _ in
-            completion()
-        })
-    }
-    
-    func openMapForPlace(raceTrack: RaceTracLocationInfo) {
-        
-        let lat1 : NSString = String(raceTrack.lat) as NSString
-        let lng1 : NSString = String(raceTrack.log) as NSString
-        
-        let latitude:CLLocationDegrees =  lat1.doubleValue
-        let longitude:CLLocationDegrees =  lng1.doubleValue
-        
-        let regionDistance:CLLocationDistance = 10000
-        let coordinates = CLLocationCoordinate2DMake(latitude, longitude)
-        let regionSpan = MKCoordinateRegion(center: coordinates, latitudinalMeters: regionDistance, longitudinalMeters: regionDistance)
-        let options = [
-            MKLaunchOptionsMapCenterKey: NSValue(mkCoordinate: regionSpan.center),
-            MKLaunchOptionsMapSpanKey: NSValue(mkCoordinateSpan: regionSpan.span)
-        ]
-        let placemark = MKPlacemark(coordinate: coordinates, addressDictionary: nil)
-        let mapItem = MKMapItem(placemark: placemark)
-        mapItem.name = "\(raceTrack.city)"
-        mapItem.openInMaps(launchOptions: options)
-        
-    }
-    
-    func getNewData(){
-        let locationsDataString = UserDefaults.standard.string(forKey: "LOCATIONS_DATA")
-        
-        if let reversedData = locationsDataString!.data(using: .utf8),
-            let reversedArray = try? JSONDecoder().decode([RaceTracLocationInfo].self, from: reversedData) {
-            self.raceTracksInfo = reversedArray
-        }
-        
     }
     
     @objc func handleSelectedObject(_ notification: Notification) {
@@ -92,32 +65,52 @@ public class CarPlaySceneDelegate: UIResponder  {
             }
         }
     }
-}
-
-// MARK: - CPTemplateApplicationSceneDelegate
-extension CarPlaySceneDelegate: CPTemplateApplicationSceneDelegate {
-
-    public func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene, didConnect interfaceController: CPInterfaceController) {
-
-        self.interfaceController = interfaceController
-        self.interfaceController?.delegate = self
     
-        // Notifications
-        NotificationCenter.default.addObserver(self, selector: #selector(handleSelectedObject(_:)),
-                name: Notification.Name("SampleAccelloriOSFrameworkDidSelectRow"), object: nil)
-        self.getNewData()
-        listViewTemplate.updateSections([updateRaceTracksList()])
-        listViewTemplate.tabTitle = "Locations"
-        // listViewTemplate.tabImage = UIImage(named: "locations")
-        let tabBar = CPTabBarTemplate.init(templates: [listViewTemplate ])
-        tabBar.delegate = self
-        self.interfaceController?.setRootTemplate(tabBar, animated: true, completion: {_, _ in })
-    
+    func selectedPop(raceTrack: RaceTracLocationInfo, completion: @escaping () -> Void) {
+        setupMap(raceTrack: raceTrack)
     }
+    
+    func setupMap(raceTrack: RaceTracLocationInfo) {
 
-    // CarPlay disconnected
-    private func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene, didDisconnect interfaceController: CPInterfaceController) {
-        self.interfaceController = nil
+        let coordinates = CLLocationCoordinate2DMake(CLLocationDegrees(raceTrack.lat), CLLocationDegrees(raceTrack.log))
+        let placemark = MKPlacemark(coordinate: coordinates, addressDictionary: nil)
+        let mapItem = MKMapItem(placemark: placemark)
+        
+        let poi = CPPointOfInterest(
+            location: mapItem,
+            title: raceTrack.city,
+            subtitle: "Store:" + raceTrack.storeId,
+            summary: raceTrack.address,
+            detailTitle: raceTrack.city,
+            detailSubtitle: "Store:" + raceTrack.storeId,
+            detailSummary: raceTrack.address,
+            pinImage: UIImage()
+        )
+        
+        poi.primaryButton = CPTextButton(
+            title: "GO",
+            textStyle: .normal,
+            handler: { _ in  }
+        )
+        
+        let pointOfInterestTemplate = CPPointOfInterestTemplate(
+            title: "Location",
+            pointsOfInterest: [poi],
+            selectedIndex: NSNotFound)
+        pointOfInterestTemplate.pointOfInterestDelegate = self
+        pointOfInterestTemplate.tabTitle = "Map"
+        pointOfInterestTemplate.tabImage = UIImage(systemName: "car")!
+    
+//        let tabTemplate = CPTabBarTemplate(templates: [pointOfInterestTemplate])
+    
+//        self.interfaceController?.setRootTemplate(tabTemplate, animated: true, completion: { (done, error) in
+//        })
+        self.interfaceController?.pushTemplate(pointOfInterestTemplate, animated: true, completion: { (done, error) in
+        })
+    }
+    
+    public func pointOfInterestTemplate(_ pointOfInterestTemplate: CPPointOfInterestTemplate, didChangeMapRegion region: MKCoordinateRegion) {
+    
     }
 }
 
@@ -147,3 +140,87 @@ extension CarPlaySceneDelegate: CPInterfaceControllerDelegate {
         print("templateDidDisappear", aTemplate)
     }
 }
+
+//import CarPlay
+//import MapKit
+//
+//public class CarPlaySceneDelegate: UIResponder, CPTemplateApplicationSceneDelegate, CPPointOfInterestTemplateDelegate  {
+//
+//    // MARK: - Properties
+//    var interfaceController: CPInterfaceController?
+//    var raceTracksInfo = [RaceTracLocationInfo]()
+//
+//    let listViewTemplate: CPListTemplate = CPListTemplate(title: "Locations", sections: [])
+//
+//    func setupMap() {
+//        let pointOfInterestTemplate = CPPointOfInterestTemplate(
+//            title: "Hoagie Options",
+//            pointsOfInterest: [],
+//            selectedIndex: NSNotFound)
+//        pointOfInterestTemplate.pointOfInterestDelegate = self
+//        pointOfInterestTemplate.tabTitle = "Map"
+//        pointOfInterestTemplate.tabImage = UIImage(systemName: "car")!
+//
+//        let tabTemplate = CPTabBarTemplate(templates: [pointOfInterestTemplate])
+//
+//        self.interfaceController?.setRootTemplate(tabTemplate, animated: true, completion: { (done, error) in
+//        })
+//    }
+//
+//    public func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene,
+//        didConnect interfaceController: CPInterfaceController) {
+//
+//        self.interfaceController = interfaceController
+//        setupMap()
+//
+//    }
+//
+//    public func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene, didDisconnectInterfaceController interfaceController: CPInterfaceController) {
+//
+//        self.interfaceController = nil
+//
+//    }
+//
+//    public func pointOfInterestTemplate(_ pointOfInterestTemplate: CPPointOfInterestTemplate, didChangeMapRegion region: MKCoordinateRegion) {
+//
+//    }
+//
+//}
+//
+//// MARK: - CPTabBarTemplateDelegate
+//extension CarPlaySceneDelegate: CPTabBarTemplateDelegate {
+//    public func tabBarTemplate(_ tabBarTemplate: CPTabBarTemplate, didSelect selectedTemplate: CPTemplate) {
+//
+//    }
+//}
+//
+//// MARK: - CPInterfaceControllerDelegate
+//extension CarPlaySceneDelegate: CPInterfaceControllerDelegate {
+//
+//    public func templateWillAppear(_ aTemplate: CPTemplate, animated: Bool) {
+//        print("templateWillAppear", aTemplate)
+//    }
+//
+//    public func templateDidAppear(_ aTemplate: CPTemplate, animated: Bool) {
+//        print("templateDidAppear", aTemplate)
+//    }
+//
+//    public func templateWillDisappear(_ aTemplate: CPTemplate, animated: Bool) {
+//        print("templateWillDisappear", aTemplate)
+//    }
+//
+//    public func templateDidDisappear(_ aTemplate: CPTemplate, animated: Bool) {
+//        print("templateDidDisappear", aTemplate)
+//    }
+//}
+
+//func selectedPop(raceTrack: RaceTracLocationInfo, completion: @escaping () -> Void) {
+//    let okAlertAction: CPAlertAction = CPAlertAction(title: "Navigate", style: .default) { _ in
+//        self.interfaceController?.dismissTemplate(animated: true, completion: { _, _ in })
+//    }
+//    let titleAlert = raceTrack.city + "," + raceTrack.address + "," + "Store " + raceTrack.storeId
+//    let alertTemplate: CPAlertTemplate = CPAlertTemplate(titleVariants: [titleAlert], actions: [okAlertAction])
+//    self.interfaceController?.presentTemplate(alertTemplate, animated: true, completion: { _, _ in
+//        completion()
+//    })
+//}
